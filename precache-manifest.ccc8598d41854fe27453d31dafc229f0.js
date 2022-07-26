aaf74fe99872a0119cc9224b09da9a1c8022b610
PepperMcLean <jackmcl206@gmail.com>
2022-07-26 08:56:50 -0500

    Updates

---
+++ precache-manifest.ccc8598d41854fe27453d31dafc229f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa2b911253829fc19a4cbe9511a13df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f53c25f65506415105a",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "34b95614c458ae6fb7aa",
+    "url": "/static/css/main.34e3bd82.chunk.css"
+  },
+  {
+    "revision": "2f53c25f65506415105a",
+    "url": "/static/js/2.838c8a8d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.838c8a8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b95614c458ae6fb7aa",
+    "url": "/static/js/main.0d7d0c83.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
